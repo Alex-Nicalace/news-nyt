c54dc6a0054c92ff5e4e7553694ab41418e3d3c6
Alex-Nicalace <anicalace@gmail.com>
2025-09-07 18:24:47 +0300

    feat: add BurgerButton component

---
+++ src/widgets/header/ui/BurgerButton/BurgerButton.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+
+import styles from './BurgerButton.module.scss';
+
+type BurgerButtonProps = {
+  className?: string;
+};
+
+export default function BurgerButton({ className }: BurgerButtonProps) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <button
+      className={clsx(styles.burger, className, { [styles.active]: open })}
+      onClick={() => setOpen(!open)}
+    >
+      <span className={styles.icon}>
+        <span></span>
+      </span>
+    </button>
+  );
+}
