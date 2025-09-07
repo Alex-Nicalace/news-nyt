e7135e15942c259ba9a32c09219f9880aa3b10ca
Alex-Nicalace <anicalace@gmail.com>
2025-09-07 19:09:04 +0300

    feat:  Header component usage Container and BurgerButton, styles

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
-import { Title } from 'shared/ui';
+import { Container, Title } from 'shared/ui';
+import { BurgerButton } from './BurgerButton';
 import styles from './Header.module.scss';
 
 type HeaderProps = {
@@ -8,7 +9,12 @@ type HeaderProps = {
 export default function Header({ className }: HeaderProps) {
   return (
     <header className={clsx(styles.Header, className)}>
-      <Title as="h1">BESIDER</Title>
+      <Container>
+        <BurgerButton className={styles.burger} />
+        <Title className={styles.title} as="h1">
+          BESIDER
+        </Title>
+      </Container>
     </header>
   );
 }
