e31d4cbaee67128b01a1ddb262f4c61f2bc3aea6
Alex-Nicalace <anicalace@gmail.com>
2025-09-08 09:14:05 +0300

    refactor: NewsCard component layout and styles

--- src/entities/news/ui/NewsCard.tsx
+++ src/entities/news/ui/NewsCard.tsx
@@ -20,15 +20,17 @@ export default function NewsCard({
 }: NewsCardProps) {
   return (
     <article className={clsx(styles.NewsCard, className)}>
-      <a className={styles.link} href={href}>
-        <h3 className={styles.title}>{title}</h3>
-        <img className={styles.img} src={image} loading="lazy" alt="" />
-        <p className={styles.text}>{text}</p>
-        <time className={styles.date} dateTime="">
-          {/* // TODO: dateTime YYYY-MM-DDThh:mm:ssTZD */}
-          {date}
-        </time>
-      </a>
+      <h3 className={styles.title}>
+        <a className={styles.link} href={href}>
+          {title}
+        </a>
+      </h3>
+      <img className={styles.img} src={image} loading="lazy" alt="" />
+      <p className={styles.text}>{text}</p>
+      <time className={styles.date} dateTime="">
+        {/* // TODO: dateTime YYYY-MM-DDThh:mm:ssTZD */}
+        {date}
+      </time>
     </article>
   );
 }
